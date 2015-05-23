5b362d1b182eeefda805aeb4e0d8839910e74b9d
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-05-23 22:32:23 +0200

    Finish Runge-Kutta 4 solver
    Stable up to about dt = 3.4

--- Project1/code/Solver.cpp
+++ Project1/code/Solver.cpp
@@ -8,6 +8,7 @@ extern int solver;
 
 void Euler(std::vector<Particle*> pVector, std::vector<Force*> fVector, float dt);
 void Midpoint(std::vector<Particle*> pVector, std::vector<Force*> fVector, float dt);
+void RungeKutta(std::vector<Particle*> pVector, std::vector<Force*> fVector, float dt);
 
 void CalcForces(std::vector<Particle*> pVector, std::vector<Force*> fVector);
 
@@ -19,6 +20,8 @@ void simulation_step( std::vector<Particle*> pVector, std::vector<Force*> fVecto
 		Euler(pVector, fVector, dt);
 	if (solver == 2)
 		Midpoint(pVector, fVector, dt);
+	if (solver == 3)
+		RungeKutta(pVector, fVector, dt);
 }
 
 
@@ -61,6 +64,70 @@ void Midpoint(std::vector<Particle*> pVector, std::vector<Force*> fVector, float
 	{
         	pVector[i]->m_Position = old_pos[i] + dt * pVector[i]->m_Velocity;
 		pVector[i]->m_Velocity = old_vel[i] + dt * pVector[i]->m_ForceVector / pVector[i]->m_Mass;
+	}
+}
+
+void RungeKutta(std::vector<Particle*> pVector, std::vector<Force*> fVector, float dt)
+{
+	int psize = pVector.size();
+
+	std::vector<Vec2f> begin_pos;
+	std::vector<Vec2f> begin_vel;
+	std::vector<Vec2f> k1, k2, k3, k4;
+	std::vector<Vec2f> l1, l2, l3, l4;
+
+	for(int i=0; i < psize; i++)
+	{
+		begin_pos.push_back(pVector[i]->m_Position);
+		begin_vel.push_back(pVector[i]->m_Velocity);
+		k1.push_back(Vec2f(0.0, 0.0));
+		k2.push_back(Vec2f(0.0, 0.0));
+		k3.push_back(Vec2f(0.0, 0.0));
+		k4.push_back(Vec2f(0.0, 0.0));
+		l1.push_back(Vec2f(0.0, 0.0));
+		l2.push_back(Vec2f(0.0, 0.0));
+		l3.push_back(Vec2f(0.0, 0.0));
+		l4.push_back(Vec2f(0.0, 0.0));
+	}
+
+	CalcForces(pVector, fVector);
+	for(int i=0; i < psize; i++)
+	{
+		k1[i] = pVector[i]->m_ForceVector / pVector[i]->m_Mass;
+		l1[i] = pVector[i]->m_Velocity;
+
+        	pVector[i]->m_Position = begin_pos[i] + 0.5 * dt * l1[i];
+		pVector[i]->m_Velocity = begin_vel[i] + 0.5 * dt * k1[i];
+	}
+
+	CalcForces(pVector, fVector);
+	for(int i=0; i < psize; i++)
+	{
+		k2[i] = pVector[i]->m_ForceVector / pVector[i]->m_Mass;
+		l2[i] = pVector[i]->m_Velocity;
+
+        	pVector[i]->m_Position = begin_pos[i] + 0.5 * dt * l2[i];
+		pVector[i]->m_Velocity = begin_vel[i] + 0.5 * dt * k2[i];
+	}
+
+	CalcForces(pVector, fVector);
+	for(int i=0; i < psize; i++)
+	{
+		k3[i] = pVector[i]->m_ForceVector / pVector[i]->m_Mass;
+		l3[i] = pVector[i]->m_Velocity;
+
+        	pVector[i]->m_Position = begin_pos[i] + dt * l3[i];
+		pVector[i]->m_Velocity = begin_vel[i] + dt * k3[i];
+	}
+
+	CalcForces(pVector, fVector);
+	for(int i=0; i < psize; i++)
+	{
+		k4[i] = pVector[i]->m_ForceVector / pVector[i]->m_Mass;
+		l4[i] = pVector[i]->m_Velocity;
+	
+        	pVector[i]->m_Position = begin_pos[i] + dt/6.0 * (l1[i] + 2 * l2[i] + 2 * l3[i] + l4[i]);
+		pVector[i]->m_Velocity = begin_vel[i] + dt/6.0 * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]);
 	}
 }
 
